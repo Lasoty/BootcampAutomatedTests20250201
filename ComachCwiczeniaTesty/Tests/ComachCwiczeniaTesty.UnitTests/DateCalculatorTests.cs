6552c0264885f3dca599e3df3646cdebb8018931
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2025-02-01 14:37:31 +0100

    Testowanie dat - rozwiązanie zadania

--- ComachCwiczeniaTesty/Tests/ComachCwiczeniaTesty.UnitTests/DateCalculatorTests.cs
+++ ComachCwiczeniaTesty/Tests/ComachCwiczeniaTesty.UnitTests/DateCalculatorTests.cs
@@ -50,4 +50,42 @@ public class DateCalculatorTests
         // Assert
         result.Should().Be(3.February(2025));
     }
+
+    [Test]
+    public void GetNextBusinessDay_ShouldHandleLeapYear()
+    {
+        // Test w roku przestępnym, 28 lutego -> Następny dzień roboczy to 1 marca
+        cut.GetNextBusinessDay(new DateTime(2024, 2,
+            28)).Should().Be(new DateTime(2024, 2, 29));
+    }
+    [Test]
+    public void GetNextBusinessDay_ShouldNotChangeTimePart()
+    {
+        // Test zachowania części czasowej
+        var time = new DateTime(2024, 2, 23, 15, 30, 0); // 15:30
+        cut.GetNextBusinessDay(time).TimeOfDay.Should().Be(time.TimeOfDay);
+    }
+    [Test]
+    public void GetNextBusinessDay_ShouldBeAfterInputDate()
+    {
+        // Test, że zwracana data jest zawsze po podanej dacie
+        var date = new DateTime(2024, 2, 23);
+        cut.GetNextBusinessDay(date).Should().BeAfter(date);
+    }
+    [Test]
+    public void GetNextBusinessDay_ShouldBeCloseToInputDate()
+    {
+        // Test, że zwracana data jest blisko podanej daty (nie więcej niż 3 dni później)
+        var date = new DateTime(2024, 2, 23);
+        cut.GetNextBusinessDay(date).Should().BeCloseTo(date,
+            TimeSpan.FromDays(3));
+    }
+    [Test]
+    public void GetNextBusinessDay_ShouldBeOnWeekday()
+    {
+        // Test, że zwracana data jest dniem roboczym
+        var date = new DateTime(2024, 2, 23);
+        var nextBusinessDay = cut.GetNextBusinessDay(date);
+        nextBusinessDay.DayOfWeek.Should().NotBe(DayOfWeek.Saturday).And.NotBe(DayOfWeek.Sunday);
+    }
 }
